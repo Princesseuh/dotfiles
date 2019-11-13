73b49a6a77e24fb701e8cf7e59ab49fc9cc3fcc9
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2019-11-13 17:07:40 -0500

    Update 2019-11-13 (Separate Fish settings)

---
+++ fish/conf.d/fzf.fish
@@ -0,0 +1,3 @@
+# fish-fzf settings
+set -gx FZF_LEGACY_KEYBINDINGS 0
+set -gx FZF_ENABLE_OPEN_PREVIEW 1
--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,8 @@
+# Fish settings
 set fish_greeting ""
 set fish_color_search_match --background='333'
 
-# Change fisher path so don't put stuff installed by fisher in our VCS
-set -g fisher_path $HOME/.config/fish/fisher
-
-set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
-set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
-
+# Load Fisher plugins
 for file in $fisher_path/conf.d/*.fish
     builtin source $file 2> /dev/null
 end

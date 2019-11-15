0ad44879ed319718fc4f086abfb7fbbc0c903c2d
Princesseuh <princssdev@gmail.com>
2019-11-15 17:26:24 -0500

    Update 2019-11-15 (README update, fd + fzf, wallpaper)

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -1,3 +1,5 @@
 # fish-fzf settings
 set -gx FZF_LEGACY_KEYBINDINGS 0
 set -gx FZF_ENABLE_OPEN_PREVIEW 1
+
+set -U FZF_FIND_FILE_COMMAND "fd --type f --hidden --no-ignore-vcs --exclude ".git" . \$dir"

a42bacb3d3db0f031a79a3083688ef6252317662
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2019-11-19 09:22:51 -0500

    Git abbreviations for fish, remove unused files

--- fish/conf.d/abbreviations.fish
+++ fish/conf.d/abbreviations.fish
@@ -1,4 +1,9 @@
 if status --is-interactive
-    abbr --global         c             clear
-    abbr --global         dots          cd $HOME/dotfiles
+    abbr    --global        c               clear
+    abbr    --global        dots            cd $HOME/dotfiles
+
+    # Git stuff
+    abbr    --global        gs              git status
+    abbr    --global        ga.             git add .
+    abbr    --global        gaa             git add -A
 end

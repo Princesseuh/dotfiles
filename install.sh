bea4686b04a809da01d1a897efd59b7756519b94
Erika <3019731+Princesseuh@users.noreply.github.com>
2020-06-24 21:26:58 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 # List of packages to install
 # Pre-Wayland packages: "i3-gaps" "picom" "dunst" "nitrogen"
 
-packages=("sway" "swaylock" "waybar" "alacritty" "rofi" "fish" "bat" "fd" "fzf" "xorg-server-xwayland", "nerd-fonts-fira-code")
+packages=("fish" "sway" "swaylock" "waybar" "alacritty" "rofi" "fish" "bat" "fd" "fzf" "xorg-server-xwayland" "nerd-fonts-fira-code")
 
 echo "Install packages? This will install the following packages and their dependencies using pacman. This requires sudo (y/n):"
 printf '%s\n' "${packages[@]}"

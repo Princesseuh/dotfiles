f6b2514da122be9a2ee80d03fca4a981003a80c5
Princesseuh <princssdev@gmail.com>
2020-08-02 03:41:14 -0400

    [fish] Only load wal theme if interactive. Fixes FZF's previews

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,6 @@
-cat ~/.cache/wal/sequences &
+if status --is-interactive
+    cat ~/.cache/wal/sequences &
+end
 
 # Start Sway at login
 if status is-login
@@ -21,18 +23,20 @@ set fish_pager_color_description 928181
 set fish_pager_color_completion d1b9b9
 set fish_pager_color_progress d1b9b9 --background='48425D'
 
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-else
-    # Load Fisher plugins
-    for file in $fisher_path/conf.d/*.fish
-        builtin source $file 2> /dev/null
+if status --is-interactive
+    if not functions -q fisher
+        set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+        curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+        fish -c fisher
+    else
+        # Load Fisher plugins
+        for file in $fisher_path/conf.d/*.fish
+            builtin source $file 2> /dev/null
+        end
     end
+
+    # Load local config
+    source $HOME/.config/fish/local.fish
+
+    starship init fish | source
 end
-
-# Load local config
-source $HOME/.config/fish/local.fish
-
-starship init fish | source

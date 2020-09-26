0ee80a264e1b8b1c59911110e871faa783ad261e
Princesseuh <princssdev@gmail.com>
2020-09-25 22:22:19 -0400

    [waybar] Test for internet before starting updates module

--- waybar/modules/updates.sh
+++ waybar/modules/updates.sh
@@ -2,6 +2,16 @@
 
 no_updates_icon=""
 updates_icon=""
+no_internet_icon=""
+
+_c=1
+while ! ping -c 1 -W 1 1.1.1.1 -q > /dev/null; do
+    text="$no_internet_icon Inget internet"
+    ((_c++)) && ((_c==15)) && echo -e "{\"text\":\""$text"\", \"class\":\"no-updates\"}" && exit
+
+    sleep 1
+done
+
 
 if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
     updates_arch=0

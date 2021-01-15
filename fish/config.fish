2d91456c1118c80824accfcda82aaff9447d4f61
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2021-01-15 02:41:28 -0500

    [fish] Update fisher to 4.1

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,3 @@
-if status --is-interactive
-    cat ~/.cache/wal/sequences &
-end
-
 # Start Sway at login
 if status is-login
     if test -z "$DISPLAY" -a $XDG_VTNR = 1
@@ -24,19 +20,13 @@ set fish_pager_color_completion d1b9b9
 set fish_pager_color_progress d1b9b9 --background='48425D'
 
 if status --is-interactive
-    if not functions -q fisher
-        set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-        curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-        fish -c fisher
-    else
-        # Load Fisher plugins
-        for file in $fisher_path/conf.d/*.fish
-            builtin source $file 2> /dev/null
-        end
+    cat ~/.cache/wal/sequences &
+
+    # Load Fisher plugins
+    for file in $fisher_path/conf.d/*.fish
+        builtin source $file 2>/dev/null
     end
 
     # Load local config
     source $HOME/.config/fish/local.fish
-
-    starship init fish | source
 end

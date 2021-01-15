762ac99631c3bbe3fcee747d9a3fdcea960ddff8
Princesseuh <princssdev@gmail.com>
2021-01-15 02:42:45 -0500

    [fish] Use function to define abbreviations for better performance

---
+++ fish/functions/add_abreviations.fish
@@ -0,0 +1,25 @@
+# We use a function to declare our abbreviations instead of simply putting them in a file in conf.d or config.fish because
+# adding all our abbreviations currently add 10ms to the startup time of Fish, I'd rather avoid it
+
+function add_abbreviations --description 'Add our abbreviations to Fish'
+    abbr    --add         c               clear
+
+    # cd shortcuts
+    abbr    --add         dots            cd $HOME/dotfiles
+    abbr    --add         work            cd $HOME/work
+
+    # Git stuff
+    abbr    --add         gs              git status
+    abbr    --add         ga              git add
+    abbr    --add         ga.             git add .
+    abbr    --add         gaa             git add -A
+    abbr    --add         gm              git commit -m
+    abbr    --add         gca             git commit --amend
+    abbr    --add         gp              git push
+    abbr    --add         gd              git diff
+    abbr    --add         cg              "cd (git rev-parse --show-toplevel)"
+
+    # Misc
+    abbr    --add         m               math
+    abbr    --add         eng             LC_ALL=C
+end
